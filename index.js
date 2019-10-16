cf3c6df58205f59174b15b98ea93e9c874f10575
Craig Baldwin <craig@wemakewebsites.com>
2019-10-16 10:40:16 +0100

    Updated error log, added success message

--- index.js
+++ index.js
@@ -20,12 +20,9 @@ currentBranchName().then(branchName => {
 
   if (validBranchNameRegExp.test(branchName)) {
     process.exitCode = 0;
+    console.log(`%c✔   ${branchName}%c matches branch naming convention`, 'color: green', 'color: white');
   } else {
     process.exitCode = 1;
-    console.error(
-      `Current Git branch name: "${branchName}" \n` +
-        `Doesn't match supplied RegExp: "${validBranchNameRegExp.toString()}"\n\n` +
-        `YOU HAVE BEEN REPORTED TO THE AUTHORITIES\n`
-    );
+    console.error(`%c✖   ${branchName}%c does not match branch naming convention %c[feature|bugfix|hotfix|support]/[location]-[name]-[new|modified|fix]`, 'color: red', 'color: white', 'color: grey');
   }
 });
