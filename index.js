974119a9b3086ff7dc6559ebd83d6131c788193b
Craig Baldwin <craigsbaldwin@gmail.com>
2021-06-24 16:06:20 +0100

    FIX: Jira format not testing correctly

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ currentBranchName().then(branchName => {
   let validBranchNameRegExp;
 
   try {
-    validBranchNameRegExp = new RegExp('^(master|staging|development|(bugfix|feature|hotfix|release)\/[A-Z]*-\d*-\w*$)', 'g');
+    validBranchNameRegExp = new RegExp('^master|staging|development|((bugfix|feature|hotfix|release)\/[A-Z]+-\d+-\w{5,})$', 'g');
   } catch (error) {
     console.error(`${error.message}\n`);
     process.exitCode = 1;
@@ -23,6 +23,6 @@ currentBranchName().then(branchName => {
     console.log(`${green}✔   ${branchName}${nc} branch naming convention correctly applied`);
   } else {
     process.exitCode = 1;
-    console.error(`${red}✖   ${branchName}${nc} branch naming convention not followed ${grey}[bugfix|feature|hotfix|release]/[[key]-[id]-[name]${nc}`);
+    console.error(`${red}✖   ${branchName}${nc} branch naming convention not followed ${grey}[bugfix|feature|hotfix|release]/[key-id]-[name]${nc}`);
   }
 });
